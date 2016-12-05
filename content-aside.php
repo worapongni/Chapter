264b319a19cd0940035c85a9e9a339a48fdfa063
Worapong <worapongni@gmail.com>
2016-12-05 19:36:16 +0700

    Revert "Add some info in Index file."
    This reverts commit 7a97666ac26497b05fbed621836c4c7174b96c61.

--- content-aside.php
+++
@@ -1,5 +0,0 @@
-<article class="post post-aside">
-	<p class="mini-meta"><?php the_author(); ?> @ <?php the_time('F j, Y'); ?></p>
-
-	<?php the_content(); ?>
-</article>
--- content-link.php
+++
@@ -1,3 +0,0 @@
-<article class="post post-link">
-	<a href="<?php echo get_the_content(); ?>"><?php the_title(); ?></a>
-</article>
